1219aaab0fe16730c895df88290d8340045f4c52
Jean Park <qkrwls5998@gmail.com>
2021-11-19 00:39:54 +1300

    mod

---
+++ phone-book/src/components/PhoneForm.js
@@ -0,0 +1,36 @@
+// file: src/components/PhoneForm.js
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+  state = {
+    name: '',
+    phone: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="이름"
+          value={this.state.name}
+          onChange={this.handleChange}
+          name="name"
+        />
+        <input
+          placeholder="전화번호"
+          value={this.state.phone}
+          onChange={this.handleChange}
+          name="phone"
+        />
+        <button type="submit">등록</button>
+
+      </form>
+    );
+  }
+}
+
+export default PhoneForm;
